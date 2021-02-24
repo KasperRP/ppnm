770d71cc5ad05cefdbde362d4d2abd413c497c0d
KasperRP <201709218@post.au.dk>
2021-02-24 10:31:02 +0100

    Plot exercise done

--- exercises/plots/main.c
+++ exercises/plots/main.c
@@ -7,7 +7,7 @@ double myerf(double);
 double mygam(double);
 
 int main() {
-	double xmin=1, xmax=3;
+	double xmin=0.1, xmax=3;
 	for(double x=xmin; x<=xmax; x+=1.0/8){
 		printf("%10g %10g %10g %10g %10g %10g %10g\n",x,erf(x),gsl_sf_erf(x),myerf(x),tgamma(x),gsl_sf_gamma(x),mygam(x));
 	}
