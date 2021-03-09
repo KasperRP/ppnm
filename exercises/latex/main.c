158a560983eca5f2953867cc5e636233392d6b8f
KasperRP <201709218@post.au.dk>
2021-03-09 14:05:49 +0100

    Added LaTeX exercise

--- exercises/latex/main.c
+++ exercises/latex/main.c
@@ -1,3 +1,11 @@
 #include<stdio.h>
+#include<math.h>
+double ex(double x);
 
-
+int main(){
+	int xmin=-2, xmax=2;
+	double x;
+	for(x=xmin;x<=xmax;x+=1.0/8)
+		printf("%g %g %g\n",x,ex(x),exp(x));
+return 0;
+}
