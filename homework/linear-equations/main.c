41be7e1cb977b250320fc2b58bc15b41a97a5c77
KasperRP <201709218@post.au.dk>
2021-03-23 22:24:46 +0100

    Exercise B added

--- homework/linear-equations/main.c
+++ homework/linear-equations/main.c
@@ -10,6 +10,8 @@ void GS_decomp(gsl_matrix* A, gsl_matrix* R);
 
 void GS_solve(gsl_matrix* Q, gsl_matrix* R, gsl_vector* b, gsl_vector* x);
 
+void GS_inverse(gsl_matrix* Q, gsl_matrix* R, gsl_matrix* X);
+
 // Defining functions for printing matrices and vectors
 
 void matrix_print(gsl_matrix* A, FILE* list){
