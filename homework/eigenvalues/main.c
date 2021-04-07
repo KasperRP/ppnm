d7a3d59c6c91f6f7d933e775eb8e2e86c11ece31
KasperRP <201709218@post.au.dk>
2021-04-07 15:24:46 +0200

    Eigenvalue B added

--- homework/eigenvalues/main.c
+++ homework/eigenvalues/main.c
@@ -69,6 +69,55 @@ int main(){
 	matrix_print(VTV, jacobi_diag_file);
 	fprintf(jacobi_diag_file, "\n \n");
 	matrix_print(VVT, jacobi_diag_file);
+	
+
+
+	// Now the Quantum particle in box problem
+	
+	FILE* quantum_eigVal_file = fopen("quantum_eigVal.txt", "w");
+	FILE* quantum_eigfunc_file = fopen("quantum_eigfunc.txt","w");
+	// Built Hamiltonian
+	
+	int n=20;
+	double s=1.0/(n+1);
+	gsl_matrix* H = gsl_matrix_alloc(n,n);
+	for(int i=0; i<n-1; i++){
+		gsl_matrix_set(H,i,i,-2);
+		gsl_matrix_set(H,i,i+1,1);
+		gsl_matrix_set(H,i+1,i,1);
+	}
+	gsl_matrix_set(H,n-1,n-1,-2);
+	gsl_matrix_scale(H,-1/s/s);
+
+	// Diagonalize the matrix
+	
+	gsl_matrix* VQ = gsl_matrix_alloc(n,n);
+	gsl_matrix_set_identity(VQ);
+	jacobi_diag(H,VQ);
+
+	// Printing the eigenvalues of H corresponding to the energies of the system
+	for (int k=0; k<n/3; k++){
+		double exact = M_PI*M_PI*(k+1)*(k+1); // see paper in the map for this
+		double calculated = gsl_matrix_get(H,k,k);
+		fprintf(quantum_eigVal_file, "n=%i calculated=%g exact=%g\n", k+1, calculated, exact);
+	}
+
+
+	// Printing obtained eigenfunctions and compare to ananlytic solution sqrt(2/L)*sin(m*pi*ksi);
+	double C = sqrt(2.0/(n+1));
+	for(int k=0; k<3; k++){
+		fprintf(quantum_eigfunc_file,"0 0 0\n");
+	for(int i=0;i<n;i++){
+		double ksi = (i+1.0)/(n+1);
+		fprintf(quantum_eigfunc_file,"%g %g %g\n", ksi, sin((k+1)*M_PI*ksi), gsl_matrix_get(VQ, i, k)*pow(-1,k)/C);} // we multiply with (-1)^k such that the phases of our eigenfunctions agree with sin(x). And divide by C for normalization
+       fprintf(quantum_eigfunc_file, "1 0 0\n");
+	}       
+
+
+
+	
+
+
 
 
 	
@@ -78,7 +127,13 @@ int main(){
 	gsl_matrix_free(VDVT);
 	gsl_matrix_free(VTV);
 	gsl_matrix_free(VVT);
+	
+	gsl_matrix_free(H);
+	gsl_matrix_free(VQ);
+
 	fclose(jacobi_diag_file);
+	fclose(quantum_eigVal_file);
+	fclose(quantum_eigfunc_file);
 	return 0;
 }
 
