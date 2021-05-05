80abc769c7bcee68cddc84a0550dc0bbc42bee3f
KasperRP <201709218@post.au.dk>
2021-05-05 16:37:47 +0200

    Num-integ added

--- homework/num-integ/integrator.c
+++ homework/num-integ/integrator.c
@@ -2,7 +2,9 @@
 #include<assert.h>
 #include<stdio.h>
 
-double adaptint(
+
+
+double quad(
 	double f(double x), 
 	double a, 
 	double b, 
@@ -21,10 +23,11 @@ double error = fabs(Q-q);
 
 if(error < tolerance){ return Q;} // We accept if error is below tolerance
 else{
-	double Q1=adaptint(f, a, (a+b)/2, acc/sqrt(2), eps, f1, f2, nrec+1);
-	double Q2=adaptint(f, (a+b)/2, b, acc/sqrt(2), eps, f3, f4, nrec+1);
+	double Q1=quad(f, a, (a+b)/2, acc/sqrt(2), eps, f1, f2, nrec+1);
+	double Q2=quad(f, (a+b)/2, b, acc/sqrt(2), eps, f3, f4, nrec+1);
 	return Q1+Q2;} // If not accepted, we split the interval into two and recur once again
 }
+
 
 double integrate(
 		double f(double x),
@@ -35,7 +38,43 @@ double integrate(
 	double f2=f(a+2*(b-a)/6);
 	double f3=f(a+4*(b-a)/6);
 	int nrec=0;
-	return adaptint(f, a, b, acc, eps, f2, f3, nrec);
+	return quad(f, a, b, acc, eps, f2, f3, nrec);
+}
+
+
+// Implementing an integral routine based on the Cleanshaw-Curtis transformation
+
+static double A;
+static double B; // Not accessible from other files
+
+double F(double f(double), double t){ // Transformed function
+
+	return f((A+B)/2+(A-B)/2*cos(t))*sin(t)*(B-A)/2;
+}
+
+double CCquad( // Just like quad but use F instead of f
+	double f(double x),
+	double a,
+	double b,
+	double acc,
+	double eps,
+	double f2,
+	double f3,
+	int nrec){
+	assert(nrec<1000000); // max number of recurrings
+	double f1 = F(f, a+(b-a)/6);
+	double f4 = F(f, a+5*(b-a)/6);
+	double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // Higher order quadrature
+	double q = (f1+f4+f2+f3)/4*(b-a); // Lower order
+	double tolerance = acc+eps*fabs(Q);
+	double error = fabs(Q-q);
+
+	if(error < tolerance) return Q;
+	else{
+		double Q1 = CCquad(f, a, (a+b)/2, acc/sqrt(2), eps, f1, f2, nrec+1);
+		double Q2 = CCquad(f, (a+b)/2, b, acc/sqrt(2), eps, f3, f4, nrec+1);
+		return Q1+Q2;
+	}
 }
 
 
@@ -45,59 +84,29 @@ double CCintegrate( // Clenshaw-Curtis integral
 		double b,
 		double acc,
 		double eps){
-	
-	double F(double t){
-		return f((b-a)*cos(t)/2+(b+a)/2)*sin(t)*(b-a)/2;}
+	A=a; B=b;
+	a=0; b=M_PI;
+	double f2=F(f,a+2*(b-a)/6);
+	double f3=F(f,a+4*(b-a)/6);
+	int nrec=0;
 
-	
-
-	return integrate(F,0,M_PI,acc,eps);
+	return CCquad(f,a,b,acc,eps,f2,f3,nrec);
 }
 
 
-
-
-//static double A;
-//static double B;
-//double F(double f(double), double t){
-//	return f((A+B)/2+(A-B)/2*cos(t))*sin(t)*(B-A)/2;
-//}
-
-//double CCadaptint(
-//		double f(double),
-//		double a,
-//		double b,
-//		double acc,
-//		double eps, 
-//		double f2,
-//		double f3,
-//		int nrec){
-//	assert(nrec<1000000);
-//	double f1=F(f,a+(b-a)/6);
-//	double f4=F(f,5*(b-a)/6);
-//	double Q=(2*f1+f2+f3+2*f4)/6*(b-a);
-//	double q=(f1+f4+f2+f3)/4*(b-a);
-//	double tol=acc+eps*fabs(Q);
-//	double err=fabs(Q-q);
-//	if(err<tol) {return Q;}
-//	else{
-//		double Q1=CCadaptint(f,a,(a+b)/2,acc/sqrt(2),eps,f1,f2,nrec+1);
-//		double Q2=CCadaptint(f,(a+b)/2,b,acc/sqrt(2),eps,f3,f4,nrec+1);
-//		return Q1+Q2;}
-//}
-
-//double CCintegrate(
+//double CCintegrate(  OBS: Nested functions do not work for Linux subspace in Windows!
 //		double f(double),
 //		double a,
 //		double b,
 //		double acc,
 //		double eps){
-//	A=a; B=b;
-//	a=0; b=M_PI;
-//	double f2=F(f,a+2*(b-a)/6);
-//	double f3=F(f,a+4*(b-a)/6);
-//	int nrec=0;
-//	return CCadaptint(f,a,b,acc,eps,f2,f3,nrec);
+
+//	double F(double t){
+//		return f( (a+b)/2+(a-b)/2*cos(t) )*sin(t)*(b-a)/2;}
+	
+//	return integrate(F, 0, M_PI, acc, eps);
 //}
+
+
 
 
--- homework/num-integ/main.c
+++ homework/num-integ/main.c
@@ -1,8 +1,10 @@
 #include<math.h>
 #include<assert.h>
 #include<stdio.h>
+#include<gsl/gsl_integration.h>
 
-double adaptint(
+
+double quad(
 		double f(double x),
 		double a,
 		double b,
@@ -26,47 +28,162 @@ double CCintegrate(
 		double acc,
 		double eps);
 
-int calls=0;
+// Functions to be integrated
+int calls;
 double g1(double x){
 	calls++;
-	return sqrt(x);};
+	return sqrt(x);}
 
 double g2(double x){
 	calls++;
-	return 4*sqrt(1-x*x);};
+	return 4*sqrt(1-x*x);}
 
 double g3(double x){
 	calls ++;
-	return 1/sqrt(x);};
+	return 1/sqrt(x);}
+
+double g4(double x){
+	calls ++;
+	return log(x)/sqrt(x);}
+
+// Writing functions again for GSL routine gsl_integration_qags
+
+double g1_GSL(double x, void* params){
+	calls++;
+	double alpha = *(double*) params;
+	return alpha*sqrt(x);}
+
+double g2_GSL(double x, void* params){
+	calls++;
+	double alpha = *(double*) params;
+	return alpha*4*sqrt(1-x*x);}
+
+double g3_GSL(double x, void* params){
+	calls++;
+	double alpha = *(double*) params;
+	return alpha/sqrt(x);}
+
+double g4_GSL(double x, void* params){
+	calls++;
+	double alpha = *(double*) params;
+	return alpha*log(x)/sqrt(x);}
 
 
-int main(){	// uses gcc nested functions as in the notes
+
+int main(){	 
 	
 
 	double a=0;
 	double b=1;
-	double acc=0.0001;
-	double eps=0.0001;
+	double acc=0.001;
+	double eps=0.001;
 
+	// For GSL routine
+	int limit = 10000;
+	gsl_integration_workspace* w = gsl_integration_workspace_alloc(limit);
+	double alpha = 1.0;
+	double result;
+	double error;
 
-	
-	double intg1=integrate(g1,a,b,acc,eps);
+	{
+	calls=0;
+	double Q=integrate(g1,a,b,acc,eps);
 	printf("Integral of sqrt(x) from 0 to 1\n");
-	printf("Analytical result = 2/3 \nMy numerical estimate = %g\nNumber of calls = %d\n",intg1,calls);
-
+	printf("Analytical result = 2/3 \nMy numerical estimate = %g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q-2/3));
+	}
 	
+	{
+	calls=0;
+	gsl_function G;
+	G.function = &g1_GSL;
+	G.params = &alpha;
+	gsl_integration_qags(&G, a, b, acc, eps, limit, w, &result, &error);
+	printf("GSL value = %f\nGSL number of calls = %d\nGSL error =%f\n", result, calls, error);
+	}
 
-	double intg2=integrate(g2,a,b,acc,eps);
+	{
+	calls=0;
+	double Q=integrate(g2,a,b,acc,eps);
 	printf("\nIntegral of 4*sqrt(1-x^2) from 0 to 1\n");
-	printf("Analytical result = pi \nMy numerical estimate = %g\nNumber of calls = %d\n",intg2,calls);
-	
-	double intg3=integrate(g3,a,b,acc,eps);
-	printf("\nIntegral of 1/sqrt(x) from 0 to 1\n");
-	printf("Analytical result = 2 \nMy numerical estimate = %g\nNumber of calls = %d\n",intg3,calls);
+	printf("Analytical result = pi = 3.1415926535897932384626433 \nMy numerical estimate = %0.25g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q-M_PI));
+	}
 
-	double intg4=CCintegrate(g3,a,b,acc,eps);
-	printf("\nIntegral of 1/sqrt(x) from 0 to 1 using Clenshaw-Curtis\n");
-	printf("My numerical estimate =%g\nNumber of calls = %d\n",intg4,calls);
+	{
+	calls=0;
+	gsl_function G;
+	G.function = &g2_GSL;
+	G.params = &alpha;
+	gsl_integration_qags(&G, a, b, acc, eps, limit, w, &result, &error);
+	printf("GSL value = %0.25f\nGSL number of calls = %d\nGSL error = %f\n", result, calls, error);
+	}	
+	
+	{
+	calls=0;
+	double Q=CCintegrate(g2,a,b,acc,eps);
+	printf("Using Clenshaw-Curtis:\nMy numerical estimate = %0.25g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q-M_PI));
+	printf("\nWe see that the Clenshaw-Curtis transform makes the result a little better than the adaptive routine, but not better than GSL\n");
+	}
+	
+	{
+	calls=0;
+	double Q=integrate(g3,a,b,acc,eps);
+	printf("\nIntegral of 1/sqrt(x) from 0 to 1\n");
+	printf("Analytical result = 2 \nMy numerical estimate = %g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q-2));
+	}
+	
+	{
+	calls=0;
+	gsl_function G;
+	G.function = &g3_GSL;
+	G.params = &alpha;
+	gsl_integration_qags(&G, a, b, acc, eps, limit, w, &result, &error);
+	printf("GSL value = %f\nGSL number of calls = %d\nGSL error = %f\n", result, calls, error);
+	}
+	{
+	calls=0;
+	double Q=CCintegrate(g3,a,b,acc,eps);
+	printf("Using Clenshaw-Curtis:\nMy numerical estimate = %g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q-2));
+	}
+
+	{
+	calls=0;
+	double Q=integrate(g4,a,b,acc,eps);
+	printf("\nIntegral of ln(x)/sqrt(x) from 0 to 1\n");
+	printf("Analtical result = -4 \nMy numerical estimate = %g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q+4));
+	}
+	
+	{
+	calls=0;
+	gsl_function G;
+	G.function = &g4_GSL;
+	G.params = &alpha;
+	gsl_integration_qags(&G, a, b, acc, eps, limit, w, &result, &error);
+	printf("GSL value = %f\nGSL number of calls = %d\nGSL error = %f\n", result, calls, error);
+	}
+
+	{
+	calls=0;
+	double Q=CCintegrate(g4,a,b,acc,eps);
+	printf("Using Clenshaw-Curtis:\n");
+	printf("My numerical estimate =%g\nNumber of calls = %d\n",Q,calls);
+	printf("My estimated error = %g\n", acc+eps*fabs(Q));
+	printf("Actual error = %g\n", fabs(Q+4));
+	printf("\nFor the two latter integrals with singular integrands it is clear by the number of counts\nhow much more efficient the Clenshaw-Curtis transform makes the calculation.");
+	}	
+
+	gsl_integration_workspace_free(w);
 
 	return 0;
 }
