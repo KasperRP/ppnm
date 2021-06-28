40838dd7f00d1adb9d146c93d3b8df07fdb7df09
KasperRP <201709218@post.au.dk>
2021-06-28 11:53:24 +0200

    Push

--- exam/2Dintegrator.c
+++ exam/2Dintegrator.c
@@ -14,18 +14,18 @@ double quad(
 	double f3,
 	int nrec){
 assert(nrec<1000000); // max number of recurrings
-double f1 = f(a+(b-a)/6);
-double f4 = f(a+5*(b-a)/6);
-double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // Higher order quadrature
-double q = (f1+f4+f2+f3)/4*(b-a); // Lower order
+double f1 = f(a+(b-a)/6); // f1=f(x1), x1=a+(b-a)/6 as in Eq. (48) in notes
+double f4 = f(a+5*(b-a)/6); 
+double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // Higher order quadrature - weights as in Eq. (49)
+double q = (f1+f4+f2+f3)/4*(b-a); // Lower order - weights as in Eq. (50)
 double tolerance = acc+eps*fabs(Q);
 double error = fabs(Q-q);
 
-if(error < tolerance) {return Q;} // We accept if error is below tolerance
+if(error < tolerance) {return Q;} // We accept if error is below tolerance, and use higher order quadrature as estimate of integral
 else{
 	double Q1 = quad(f, a, (a+b)/2, acc/sqrt(2), eps, f1, f2, nrec+1);
 	double Q2 = quad(f, (a+b)/2, b, acc/sqrt(2), eps, f3, f4, nrec+1);
-	return Q1+Q2;} // If not accepted, we split the interval into two and recur once again
+	return Q1+Q2;} // If not accepted, we split the interval into two and recur once again with rescaled acceptance
 }
 
 double integrate(
@@ -34,7 +34,7 @@ double integrate(
 		double b,
 		double acc,
 		double eps){
-	double f2 = f(a+2*(b-a)/6);
+	double f2 = f(a+2*(b-a)/6); 
 	double f3 = f(a+4*(b-a)/6);
 	int nrec = 0;
 	return quad(f, a, b, acc, eps, f2, f3, nrec);
@@ -54,21 +54,21 @@ double quad2D(
 	double F3,
 	int nrec){
 assert(nrec<1000000);
-double X1 = a+(b-a)/6;
+double X1 = a+(b-a)/6; // Same points as before
 double X4 = a+5*(b-a)/6;
-double f1(double y){ return F(X1, y);}
+double f1(double y){ return F(X1, y);} // Using nested functions
 double f4(double y){ return F(X4, y);}
-double F1 = integrate(f1, d(X1), u(X1), acc, eps);
+double F1 = integrate(f1, d(X1), u(X1), acc, eps); // Calculating F(Xi) ,i=1,...,4, using the 1D integrator
 double F4 = integrate(f4, d(X4), u(X4), acc, eps);
-double I = (2*F1+F2+F3+2*F4)/6*(b-a);
-double i = (F1+F4+F2+F3)/4*(b-a);
+double I = (2*F1+F2+F3+2*F4)/6*(b-a);  // Higher order quadrature - same weights as before
+double i = (F1+F4+F2+F3)/4*(b-a); // Lower order quadrature
 double tolerance = acc+eps*fabs(I);
 double error = fabs(I-i);
-if(error < tolerance) { return I;}
+if(error < tolerance) { return I;} // If accepted we use higher order quadrature as estimate of integral
 else{
 	double I1 = quad2D(F, a, (a+b)/2, d, u, acc/sqrt(2), eps, F1, F2, nrec+1);
 	double I2 = quad2D(F, (a+b)/2, b, d, u, acc/sqrt(2), eps, F3, F4, nrec+1);
-	return I1+I2;}
+	return I1+I2;} // If not accepted, we subdivide the interval into two as before
 }
 
 double integrate2D(
@@ -81,7 +81,7 @@ double integrate2D(
 		double eps){
 double X2 = a+2*(b-a)/6;
 double X3 = a+4*(b-a)/6;
-double f2(double y) {return F(X2,y);}
+double f2(double y) {return F(X2,y);} // Nested functions
 double f3(double y) {return F(X3,y);}
 double F2 = integrate(f2, d(X2), u(X2), acc, eps);
 double F3 = integrate(f3, d(X3), u(X3), acc, eps);
--- exam/main.c
+++ exam/main.c
@@ -12,15 +12,29 @@ double integrate2D(
 		double eps);
 
 
-// simple function to be integrated
+// simple functions to be integrated
+int calls;
 double Func1(double x, double y){
-       return x*y; }
+	calls++;
+       return x*y; } 
 
+double Func2(double x, double y){
+	calls++;
+	return -x/(y*y); }
+
+double Func3(double x, double y){
+	calls++;
+	return -1./(y*y); }
+
+// Limit functions for the y-integral
 double d1(double x){
  	return 1;}
 
 double u1(double x){
 	return x;}
+
+double u2(double x){
+	return sqrt(x);}
 
 int main(){
 
@@ -28,11 +42,38 @@ double a = 0;
 double b = 1;
 double acc = 0.01;
 double eps = 0.01;
-
 FILE* testfile = fopen("testfile.txt", "w");
+{
+calls=0;
 double I = integrate2D(Func1, a, b, d1, u1, acc, eps);
+fprintf(testfile, "Integral of x*y where y runs from 1 to x and x from 0 to 1:\n");
 fprintf(testfile, "My estimate = %g\n", I);
-fprintf(testfile, "Analytical result = -1/8");
+fprintf(testfile, "Analytical result = -1/8\n");
+fprintf(testfile, "acc = %g\neps = %g\n", acc, eps);
+fprintf(testfile, "Number of calls = %d\n", calls);
+}
+
+{
+calls=0;
+double I = integrate2D(Func2, a, b, d1, u1, acc, eps);
+fprintf(testfile, "\nIntegral of -x/y^2 where y runs from 1 to x and x from 0 to 1:\n");
+fprintf(testfile, "My estimate = %g\n", I);
+fprintf(testfile, "Analytical result = 1/2\n");
+fprintf(testfile, "acc = %g\neps = %g\n", acc, eps);
+fprintf(testfile, "Number of calls = %d\n", calls);
+}
+
+{
+calls=0;
+double I = integrate2D(Func3, a, b, d1, u2, acc, eps);
+fprintf(testfile, "\nIntegral of -1/y^2 where y runs from 1 to sqrt(x) and x from 0 to 1:\n");
+fprintf(testfile, "My estimate = %g\n", I);
+fprintf(testfile, "Analytical result = 1\n");
+fprintf(testfile, "acc = %g\neps = %g\n", acc, eps);
+fprintf(testfile, "Number of calls = %d\n", calls);
+fprintf(testfile, "\nFor the last integral the number of calls is expected to be large\ncompared to the others, due to the presence of a singularity. For a faster\nconvergence, one could have included the Clenshaw-Curtis transformation in\nthe one-dimensional adaptive integrator.\n"); 
+}
+
 fclose(testfile);
 return 0;
 }
